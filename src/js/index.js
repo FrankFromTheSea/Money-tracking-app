e1d0768f232c3c717d29b91f9723c0b032d94cec
Frank <frank@iMac-di-Frank.local>
2022-06-13 23:47:17 +0200

    init configuration

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+(function() {
+    var wallet;
+    function addOperation() {
+
+    }
+    function removeperation() {
+        
+    }
+    function findperation() {
+        
+    }
+    function getBalance() {
+        return wallet.getBalance();
+    }
+    function getOperations() {
+        return wallet.getOperations();
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        wallet = new Wallet();
+    })
+
+})();
---
+++ src/js/models/Wallet.js
@@ -0,0 +1,24 @@
+function Wallet() {
+    var balance = 0;
+    var operations = [];
+
+    this.addOperation = function() {
+
+    }
+
+    this.removeOperation = function() {
+        
+    }
+
+    this.findOperation = function() {
+        
+    }
+
+    this.getBalance = function() {
+        
+    }
+
+    this.getOperation = function() {
+        
+    }
+}
